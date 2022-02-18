302496d3f2c9bca474edafc640a89f128ac853fa
mattls <mattls@DESKTOP-N7GDJPK.localdomain>
2022-02-18 18:03:20 +0100

    Final solution 1, nested for loops c++

--- TwoNumberSums/twoNumberSum.cpp
+++ TwoNumberSums/twoNumberSum.cpp
@@ -4,23 +4,27 @@ using namespace std;
 
 //vector<int> twoNumberSum(vector<int> array, int targetSum) {}
 
+vector<int> twoNumberSumSoluOne(vector<int> array, int targetSum) {
+	vector<int> sum(2);
+	
+  for(int i = 0; i < array.size(); i ++){
+		int firstNumber = array[i];
+		for(int j = i + 1; j < array.size(); j++){
+			int secondNumber = array[j];
+			if((firstNumber + secondNumber) == targetSum){
+				sum[0] = firstNumber;
+				sum[1] = secondNumber;
+				return sum;
+			}
+		}
+	}
+  return {};
+}
+
 int main (){
     vector<int> sum{3, 5, -4, 8, 11, 1, -1, 6};
-    vector<int> nums(2);
-    int targetSum = 10;
+    int targetsum = 10;
 
-    for(int i = 0; i < sum.size(); i++){
-        int firstNumber = sum[i];
-        for(int j = i + 1; j < sum.size(); j++){
-            int secondNumber = sum[j];
-            if((firstNumber + secondNumber) == targetSum) {
-                nums.push_back(firstNumber);
-                nums.push_back(secondNumber);
-                for(int x : nums)
-                    cout << x << " ";
-            }
-        }
-    }
 
    return 0;
 }
